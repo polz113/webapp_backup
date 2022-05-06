394ac5846b578475bc668ded13ab18c377262feb
Gašper Fele-Žorž <polz@fri.uni-lj.si>
2022-05-06 15:08:03 +0200

    Add bacik laravel (.env) support

--- prepare_backup.sh
+++ prepare_backup.sh
@@ -3,11 +3,29 @@
 # Variables
 
 
+WEBAPP_TYPE=$1
+WEBAPP_DIR=$2
+BACKUP_DIR=$3
 
-WEBAPP_DIR=$1
-BACKUP_DIR=$2
+if [ -z "$1" ]; then
+    help="
+Create snapshot, dump database.
 
-GWC="get_web_config.php moodle $WEBAPP_DIR"
+Usage:
+$0 <app_type> <app_directory> <backup_destination>
+"
+    echo $help;
+    exit 1;
+fi
+
+if [ -f "get_${WEBAPP_TYPE}_config" ]; then
+    GWC="./get_${WEBAPP_TYPE}_config \"${WEBAPP_DIR}\""
+else
+    echo "App type ${WEBAPP_TYPE} not supported";
+    exit 1;
+fi
+
+# GWC="get_web_config $WEBAPP_DIR"
 
 DATA_DIR=$($GWC datadir)
 SNAPSHOT_DIR="$BACKUP_DIR"
@@ -18,7 +36,7 @@ SNAPSHOT_NAME="moodledata"
 # Enter maintenance mode
 $($GWC enter_maintenance_cmd)
 # Delete possible old snapshot
-btrfs subvolume delete "$SNAPSHOT_DIR/$SNAPSHOT_NAME"
+btrfs subvolume delete "${SNAPSHOT_DIR}/${SNAPSHOT_NAME}"
 # Create FS snapshot
 echo btrfs subvolume snapshot –r "$DATA_DIR" "$SNAPSHOT_DIR/$SNAPSHOT_NAME"
 btrfs subvolume snapshot -r "$DATA_DIR" "$SNAPSHOT_DIR/$SNAPSHOT_NAME"
